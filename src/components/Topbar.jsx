fba7ac5110ac4bc7db402b862def4ed76caa0405
Agam Agarwal <agam.agarwal04@gmail.com>
2025-05-17 20:41:55 +0530

    Minor changes

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Topbar() {
     return (
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center gap-2">
         <div className="flex items-center gap-2">
           <select className="border p-2 rounded text-sm">
             <option>All Candidates</option>
